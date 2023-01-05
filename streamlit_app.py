6c9f290a4cfd51eb5995a6cdd525550e8298737f
Dan Bleaken <122038434+dbleaken-sf-training@users.noreply.github.com>
2023-01-05 16:01:47 +0000

    Update streamlit_app.py
    more menu items, emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,11 @@ import streamlit
 
 streamlit.title('My parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥬 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚 Hard-Boiled Free Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 
